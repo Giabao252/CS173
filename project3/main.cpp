9e746c84fd9771e2bffe5a37924cc5116762751f
Giabao252 <danieluu06@gmail.com>
2023-02-07 15:49:37 -0500

    new project initialized

---
+++ project3/main.cpp
@@ -0,0 +1,19 @@
+/*
+
+@Authors: Bao Luu and Caroline Cavalier 
+@Description: 
+
+@Date: 2023/02/07
+@Updated: 2023/02/07
+
+*/
+
+//including libraries
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
