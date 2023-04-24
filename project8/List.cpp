e3e556df3eae002b4d4345e912a27884ab855c38
Giabao252 <danieluu06@gmail.com>
2023-04-23 21:08:24 -0400

    project8 test num1

---
+++ project8/List.cpp
@@ -0,0 +1,290 @@
+#include <iomanip>
+#include <math.h>
+#include <sstream>
+#include <iostream>
+#include "List.h"
+using namespace std;
+template <class T>
+List<T>::List(void)
+{
+    head = NULL;
+}
+
+template <class T>
+List<T>::~List(void)
+{
+    Node *next_node;
+
+    while (head != NULL)
+    {
+        next_node = head->next;
+        delete head;
+        head = next_node;
+    }
+
+    head = NULL;
+}
+
+template <class T>
+List<T>::List(const List<T> &mylist)
+{
+    Node *new_ptr;
+    Node *traverse_ptr;
+    Node *ptr;
+
+    if (mylist.head == NULL)
+    {
+        head = NULL;
+    }
+    else
+    {
+        head = new Node;
+        traverse_ptr = mylist.head;
+        head->item = traverse_ptr->item;
+        head->next = NULL;
+        ptr = head;
+        traverse_ptr = traverse_ptr->next;
+
+        while (traverse_ptr != NULL)
+        {
+            new_ptr = new Node;
+            new_ptr->item = traverse_ptr->item;
+            new_ptr->next = NULL;
+
+            ptr->next = new_ptr;
+            ptr = ptr->next;
+
+            traverse_ptr = traverse_ptr->next;
+        }
+    }
+}
+
+template <class T>
+List<T> List<T>::operator=(const List<T> &mylist)
+{
+    if (this != &mylist)
+    {
+        clear();
+
+        Node *new_ptr;
+        Node *traverse_ptr;
+        Node *ptr;
+
+        if (mylist.head == NULL)
+        {
+            head = NULL;
+        }
+        else
+        {
+            head = new Node;
+            traverse_ptr = mylist.head;
+            head->item = traverse_ptr->item;
+            head->next = NULL;
+            ptr = head;
+            traverse_ptr = traverse_ptr->next;
+
+            while (traverse_ptr != NULL)
+            {
+                new_ptr = new Node;
+                new_ptr->item = traverse_ptr->item;
+                new_ptr->next = NULL;
+
+                ptr->next = new_ptr;
+                ptr = ptr->next;
+
+                traverse_ptr = traverse_ptr->next;
+            }
+        }
+    }
+
+    return *this;
+}
+
+template <class T>
+string List<T>::to_string(void) const
+{
+    stringstream stream;
+
+    Node *current = head;
+
+    while (current != NULL)
+    {
+        stream << current->item;
+
+        if (current->next != NULL)
+        {
+            stream << ',';
+        }
+
+        current = current->next;
+    }
+
+    return stream.str();
+}
+
+template <class T>
+void List<T>::append(const T &item)
+{
+    Node *ptr;
+    Node *ptr2;
+
+    ptr = new Node;
+    ptr->item = item;
+    ptr->next = NULL;
+
+    if (head == NULL)
+    {
+        head = ptr;
+    }
+    else
+    {
+        ptr2 = head;
+        while (ptr2->next != NULL)
+        {
+            ptr2 = ptr2->next;
+        }
+        ptr2->next = ptr;
+    }
+}
+
+template <class T>
+T &List<T>::operator[](int index)
+{
+    Node *iPtr;
+    int index_counter = 0;
+    iPtr = head;
+
+    if (index < 0 || index > length() - 1)
+    {
+        throw std::invalid_argument("Invalid index");
+    }
+    if (iPtr == NULL)
+    {
+        throw std::out_of_range("List index out of range");
+    }
+    while (index_counter < index)
+    {
+        index_counter++;
+        iPtr = iPtr->next;
+    }
+    T value_of_index = iPtr->item;
+    return value_of_index;
+}
+
+template <class T>
+void List<T>::insert(const T &item, int index)
+{
+    if (index < 0 || index > length() - 1) {
+        throw std::invalid_argument("Invalid index");
+    }
+
+    if (index == 0) {
+        Node* new_node;
+        new_node = new Node;
+        new_node->item = item;
+        new_node->next = head; //linking the new node to the old first node of the list
+        head = new_node;
+    }
+
+    else if (index == length()) {
+        append(item);
+    }
+
+    else {
+        Node* current = head;
+        int index_counter = 0;
+
+        while (index_counter < index) {
+            index_counter++;
+            current = current->next;
+        }
+        Node* new_node; 
+        new_node = new Node;
+        new_node->item = item;
+
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+}
+
+template <class T>
+void List<T>::remove(int index)
+{
+    if (head == NULL)
+    {
+        throw std::out_of_range("List index out of range");
+    }
+
+    if (index < 0 || index > length() - 1) {
+        throw std::invalid_argument("Invalid index");
+    }
+
+    if (index == 0) {
+        Node* old_head = head;
+        head = head->next;
+        delete old_head;
+    }
+    else {
+        Node* to_delete;
+        Node* current = head;
+        int index_counter = 0;
+
+        while (index_counter < index) {
+            index_counter++;
+            current = current->next; 
+        }
+        
+        to_delete = current->next;
+        delete to_delete;
+        if (index == length() - 1) {
+            current->next = NULL;
+        }
+        else {
+            current->next = current->next->next;
+        }
+    }
+    
+}
+
+template <class T>
+int List<T>::length(void) const
+{
+    Node *head_ptr;
+    int size = 0;
+    head_ptr = head;
+
+    while (head_ptr != NULL)
+    {
+        size++;
+        head_ptr = head_ptr->next;
+    }
+    return size;
+}
+
+template <class T>
+bool List<T>::isEmpty(void) const
+{
+
+    if (head == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <class T>
+void List<T>::clear(void)
+{
+    Node *next_node;
+
+    while (head != NULL)
+    {
+        next_node = head->next;
+        delete head;
+        head = next_node;
+    }
+    head = NULL;
+}
--- test.cpp
+++ test.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
-#include <string>
 #include <iomanip>
 #include <math.h>
 #include <sstream>
 #include <iostream>
 using namespace std;
 
-#define DEFAULT_LIST_CAPACITY 10
-
 template <class T>
 class List
 {
-    // DO NOT CHANGE THIS PART
+private:
+    // struct for Node for linked list
+    struct Node
+    {
+        T item;
+        Node *next;
+    };
+
+    Node *head; // the pointer for the linked list
+
 public:
     List(void);
     List(const List<T> &mylist);
@@ -26,245 +31,174 @@ public:
     int length(void) const;
     bool isEmpty(void) const;
     void clear(void);
-private:
-    // DO NOT CHANGE THESE THREE VARIABLES
-    T *list;      // the pointer for the dynamic array
-    int capacity; // the size of the array
-    int size;     // the number of items in the list
 
-    // YOU CAN ADD METHODS/VARIABLES HERE
-    void add_memory()
+    friend ostream &operator<<(ostream &os, List<T> &mylist)
     {
-        capacity *= 2;
-		T *newList = new T[capacity];
+        Node *ptr = mylist.head;
+        while (ptr != NULL)
+        {
+            if (ptr->next != NULL)
+                os << ptr->item << " ";
+            else
+                os << ptr->item;
+            ptr = ptr->next;
+        }
 
-		for (int i = 0; i < capacity; i++)
-		{
-			newList[i] = list[i];
-		}
-
-		delete[] list;
-
-		list = newList;
+        return os;
     }
 };
 
-/*
-DEFAULT CONSTRUCTOR -> Creates an empty list
-PARAMETERS: none
-RETURN: none
-FUNCTIONALITY:
-- sets capacity to the default capacity defined in the header file
-- sets size to NULL
-- initializes an array with the default size, capacity and return a pointer
-to the first element of the array
-*/
+// #########################################################################################################
 
 template <class T>
 List<T>::List(void)
 {
-    capacity = DEFAULT_LIST_CAPACITY;
-    size = 0;
-    list = new T[DEFAULT_LIST_CAPACITY];
+    head = NULL;
 }
-
-/*
-COPY CONSTRUCTOR -> Deep copy
-PARAMETERS: const reference to the passed object mylist
-RETURN: none
-FUNCTIONALITY: copies the size, capacity and values of the parameter
-to the caller of the function
-*/
-
-template <class T>
-List<T>::List(const List<T> &mylist)
-{
-    list = new T[mylist.capacity];
-    capacity = mylist.capacity;
-    size = mylist.size;
-    copy(mylist.list, mylist.list + mylist.size, list);
-}
-
-/*
-DESTRUCTOR
-PARAMETERS: none
-RETURN: none
-FUNCTIONALITY: returns the array to the free store
-*/
 
 template <class T>
 List<T>::~List(void)
 {
-    delete[] list;
+    Node *next_node;
+
+    while (head != NULL)
+    {
+        next_node = head->next;
+        delete head;
+        head = next_node;
+    }
+
+    head = NULL;
 }
 
-/*
-ASSIGNMENT OPERATOR
-PARAMETERS: const reference to the passed object mylist
-RETURN: *this -> a reference to the current object
-*/
+template <class T>
+List<T>::List(const List<T> &mylist)
+{
+    Node *new_ptr;
+    Node *traverse_ptr;
+    Node *ptr;
+
+    if (mylist.head == NULL)
+    {
+        head = NULL;
+    }
+    else
+    {
+        head = new Node;
+        traverse_ptr = mylist.head;
+        head->item = traverse_ptr->item;
+        head->next = NULL;
+        ptr = head;
+        traverse_ptr = traverse_ptr->next;
+
+        while (traverse_ptr != NULL)
+        {
+            new_ptr = new Node;
+            new_ptr->item = traverse_ptr->item;
+            new_ptr->next = NULL;
+
+            ptr->next = new_ptr;
+            ptr = ptr->next;
+
+            traverse_ptr = traverse_ptr->next;
+        }
+    }
+}
 
 template <class T>
 List<T> List<T>::operator=(const List<T> &mylist)
 {
-    delete [] list; 
-	capacity = mylist.capacity; 
-	size = mylist.size; 
-	list = new T[mylist.capacity]; 
-	copy(mylist.list, mylist.list + mylist.size, list);
-	return *this; 
-}
-
-/*
-TO_STRING FUNCTION
-PARAMETERS: none
-RETURN: a string of the list elements separated by commas
-*/
-template <class T>
-string List<T>::to_string(void) const
-{
-    stringstream stream;
-    for (int i = 0; i < size; i++)
-        stream << *(list + i) << " ";
-    return stream.str();
-}
-/*
-APPEND FUNCTION
-PARAMETERS: const reference to the passed 'item' value of type T
-RETURN: none
-FUNCTIONALITY: append a new element at the end of the list
-*/
-
-template <class T>
-void List<T>::append(const T &item)
-{
-    if (size == capacity)
+    if (this != &mylist)
     {
-        add_memory();
-    };
+        Node *next_node;
 
-    T *elementPtr = list + size;
-    *elementPtr = item;
-    size++;
-}
-
-/*
-OPERATOR[]
-PARAMETERS: reference to the inputted index
-RETURN: the list's value at specified index
-*/
-
-template <class T>
-T &List<T>::operator[](int index)
-{
-    if (index < 0 || index > size - 1)
-    {
-        throw std::invalid_argument("Index out of range");
-    };
-    T *indexPtr = list + index;
-    return *indexPtr;
-}
-
-/*
-INSERT FUNCTION
-PARAMETERS:
-- const reference to 'item' of type T
-- an index of type integer
-RETURN: none
-FUNCTIONALITY: shifts all elements on the right of the inputted index 1 unit to the right
-to make space for the new item to be inserted into the specified position
-*/
-
-template <class T> 
-void List<T>::insert(const T &item, int index) {
-    if (index >= 0 || index <= size) {
-        size += 1;
-        if (size == capacity) {
-            add_memory();
+        while (head != NULL)
+        {
+            next_node = head->next;
+            delete head;
+            head = next_node;
         }
-        for (int i = size - 1; i > index; i--) {
-            list[i] = list[i - 1];
+
+        head = NULL;
+
+        Node *new_ptr;
+        Node *traverse_ptr;
+        Node *ptr;
+
+        if (mylist.head == NULL)
+        {
+            head = NULL;
         }
-        list[index] = item;
+        else
+        {
+            head = new Node;
+            traverse_ptr = mylist.head;
+            head->item = traverse_ptr->item;
+            head->next = NULL;
+            ptr = head;
+            traverse_ptr = traverse_ptr->next;
+
+            while (traverse_ptr != NULL)
+            {
+                new_ptr = new Node;
+                new_ptr->item = traverse_ptr->item;
+                new_ptr->next = NULL;
+
+                ptr->next = new_ptr;
+                ptr = ptr->next;
+
+                traverse_ptr = traverse_ptr->next;
+            }
+        }
     }
-}
-
-/*
-REMOVE FUNCTION
-PARAMETERS: an index of type integer
-RETURN: none
-FUNCTIONALITY: remove the list's element at specified inde and then shifts all elements on the right
-of the inputted index 1 unit to the left
-*/
-
-template <class T> 
-void List<T>::remove(int index) {
-    if (index >= 0 || index <= size - 1) {
-        size -= 1;
-        for (int i = index; i < size; i++) {
-            list[i] = list[i + 1];
-        } 
-    }
-}
-
-template <class T> 
-List<T> List<T>::operator+(const List<T> &mylist) const {
-    List<T> concat;
-    concat.size = 0;
-    concat.capacity = capacity + mylist.capacity;
-    concat.list = new T[concat.capacity];
-
-    for (int i = 0; i < size; i++) {
-        T* listPtr = list + i;
-        concat.append(*listPtr);
-    } 
-
-    for (int i = 0; i < mylist.size; i++) {
-        T* mylistPtr = mylist.list + i;
-        concat.append(*mylistPtr);
-    }
-    return concat;
 }
 
 template <class T>
-int List<T>::length(void) const
-{
+void List<T>::append(const T &item) {
+    Node* ptr;
+    Node* ptr2;
+
+    ptr = new Node;
+    ptr->item = item;
+    ptr->next = NULL
+
+    if (head == NULL) {
+        head = ptr;
+    }
+    else {
+        ptr2 = head;
+        while(ptr2->next != NULL) {
+            ptr2 = ptr2->next;
+        }
+        ptr2->next = ptr;
+        
+    }
+}
+
+template <class T> 
+int List<T>::length(void) const {
+    int size = 0;
+    while (head->next != NULL) {
+        head = head->next;
+        size++
+    }
     return size;
 }
 
-template <class T>
-bool List<T>::isEmpty(void) const
-{
-    if (size == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-template <class T>
-void List<T>::clear(void)
-{
-    size = 0;
-    delete[] list;
-    list = new T[capacity];
-}
+// #########################################################################################################
 
 int main(void)
 {
     List<int> list1;
-    cout << "list1 = " << list1.to_string() << endl;
+    cout << "list1 = " << list1 << endl;
 
     for (int i = 1; i <= 10; i++)
         list1.append(i);
 
-    cout << "list1 = " << list1.to_string() << endl;
+    cout << "list1 = " << list1 << endl;
 
     List<int> list2(list1);
-    cout << "list2 = " << list2.to_string() << endl;
+    cout << "list2 = " << list2 << endl;
 
     cout << "\nlist1.insert(50,10);\nlist1.insert(0,0)\n";
     cout << "list1.remove(1);\n";
@@ -272,8 +206,8 @@ int main(void)
     list1.insert(50, 10);
     list1.insert(0, 0);
     list1.remove(1);
-    cout << "list2 = " << list2.to_string() << endl;
-    cout << "list1 = " << list1.to_string() << endl;
+    cout << "list2 = " << list2 << endl;
+    cout << "list1 = " << list1 << endl;
 
     cout << "\nlist3 new empty\n";
     List<int> list3;
@@ -285,7 +219,7 @@ int main(void)
 
     cout << "\nlist3 = list1+list2\n";
     list3 = list1 + list2;
-    cout << "list3 = " << list3.to_string() << endl;
+    cout << "list3 = " << list3 << endl;
 
     cout << "list3 size = " << list3.length() << endl;
     if (list3.isEmpty())
